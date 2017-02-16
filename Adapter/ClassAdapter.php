f9a1d391fababa762fe21899fa7ab46de88d19f1
彭朝领 <576330124@qq.com>
2017-02-16 17:08:24 +0800

    适配器模式
    增加适配器模式

---
+++ Adapter/ClassAdapter.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Author: Lying
+ * Data: 2017-02-14
+ * description: php实现适配器模式
+ * 详解在我的博客：http://pengchaoling.com/a/subject/pattern/
+ */
+
+interface Target {
+	//原来的类里面有的方法
+	public function writeTxt();
+	//需要扩展的新功能的方法
+	public function writeXml();
+}
+
+
+class LogToTxt{
+	//保存需要写入的信息
+	private  $info;
+
+	function __construct($info){
+		$this->info = $info;
+	}
+	//把日志写到txt里
+	public function writeTxt(){
+		//本案例只是为了演示适配器的原理，这里不具体实现，有兴趣者可自行实现
+		//doWrite
+		echo "已经把信息：". $this->info."写入到txt文件中"; 
+
+	}
+}
+
+//新建一个适配器 把新的功能写进去，并把之前那个继承过来
+class LogAdapter extends LogToTxt implements Target{
+	private  $info;
+
+	function __construct($info){
+		parent::__construct($info);
+		$this->info = $info;
+	}
+	//直接继承自原来那个类的那啥
+	public function writeTxt(){
+		parent::writeTxt();
+	}
+
+	//适配器中新增加的方法
+	public function writeXml(){
+		echo "已经把信息：". $this->info."写入到XML文件中"; 
+	}
+}
+
+
+	//Client调用
+	$logObj = new LogAdapter("404 not found");
+	$logObj->writeTxt();	//写到txt里
+	$logObj->writeXml();	//写到xml里
+
+
+
+
+
+?>
